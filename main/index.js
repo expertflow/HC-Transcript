2e8042bfcfac6e0817854d977550ad785fecb52c
usama.shabbir <usama.shabbir@expertflow.com>
2024-07-03 19:53:57 +0500

    adding pagination

--- main/index.js
+++ main/index.js
@@ -1,35 +1,57 @@
+const api_url = history_api_url;
+var messages = [];
+// pagination varibles
+
+var options = [10, 15, 20, 25]; // page size option
+var totalPages;
+var currentPage = 1;
+var pageSize = [0];
+var searchQuery;
+
 document.getElementById("searchButton").addEventListener("click", () => {
-  const searchQuery = document.getElementById("searchInput").value;
-  if (searchQuery) {
+  const searchquery = document.getElementById("searchInput").value;
+  if (searchquery) {
+    searchQuery = searchquery;
     const page = 1; // reset to the first page for search results
     const pageSize = 10;
+    // reset ui before fetching
+    document.getElementById("msg").innerHTML = "";
+    document.querySelector(".paginationArea").innerHTML = "";
     fetchMessages(page, pageSize, searchQuery);
   }
 });
 
-const api_url = history_api_url;
-var messages = [];
-
 function fetchMessages(page, pageSize, searchQuery) {
+  showSpinner();
   const request = new XMLHttpRequest();
   let url = `${api_url}/getMessagesByRefId?refId=${searchQuery}&page=${page}&pageSize=${pageSize}`;
   request.open("GET", url);
   request.send();
   request.onload = () => {
     if (request.status === 200) {
-      
       result = JSON.parse(request.response);
       messages = result.messages;
-      console.log('Total Message: ', result.metaData.totalMessages);
-      console.log("Messages :", messages);
-      if (messages.length === 0) {
-        document.getElementById("chatDate").innerHTML = 'No Record Found';
+      if (messages) {
+        console.log("Total Message: ", result.metaData.totalMessages);
+
+        totalPages = calculateTotalPages(
+          result.metaData.totalMessages,
+          pageSize
+        );
+        if (result.metaData.totalMessages > 10) {
+          showPagination();
+        }
+        messageFunction();
+        hideSpinner();
+      } else {
+        document.getElementById("chatDate").innerHTML = "No Record Found";
+        hideSpinner();
       }
-      messageFunction();
     } else {
-      console.log(`error ${request.status} ${request.statusText}`)
+      console.log(`error ${request.status} ${request.statusText}`);
+      hideSpinner();
     }
-  }
+  };
 }
 
 //Function for Chat Messages Of BOT , AGENT and CUSTOMER
@@ -37,17 +59,18 @@ function messageFunction() {
   let chatDiv = `<div>`;
 
   for (const msg in messages) {
-
     const message = messages[msg];
     let date = message.timestamp.slice(0, 10).replace(/-/g, "/");
     let dateTime = new Date(message.timestamp); //Convert UTC without GMT dateTime to Locale with GMT
-    let min = `${dateTime.getMinutes()}` <= 9 ? `0${dateTime.getMinutes()}` : `${dateTime.getMinutes()}`;
+    let min =
+      `${dateTime.getMinutes()}` <= 9
+        ? `0${dateTime.getMinutes()}`
+        : `${dateTime.getMinutes()}`;
     let time = timeConvert(`${dateTime.getHours()}:${min}`);
 
     document.getElementById("chatDate").innerHTML = date;
 
-    if (message.from.type == 'Bot') {
-
+    if (message.from.type == "Bot") {
       chatDiv += `
           <div class="chat-message agent-message bot-message">
             <div class="profile-pic">
@@ -62,8 +85,7 @@ function messageFunction() {
             </div>
           </div>`;
     }
-    if (message.from.type == 'Agent') {
-
+    if (message.from.type == "Agent") {
       chatDiv += `
           <div class="chat-message agent-message">
             <div class="profile-pic">
@@ -78,8 +100,7 @@ function messageFunction() {
             </div>
           </div>`;
     }
-    if (message.from.type == 'Customer') {
-
+    if (message.from.type == "Customer") {
       chatDiv += `
           <div class="chat-message user-message ">
             <div class="profile-pic">
@@ -95,33 +116,117 @@ function messageFunction() {
           </div>`;
     }
   }
-  chatDiv += '</div>';
+  chatDiv += "</div>";
   document.getElementById("msg").innerHTML = chatDiv;
 }
 
 function timeConvert(time) {
   // Check correct time format and split into components
-  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [time];
+  time = time.toString().match(/^([01]\d|2[0-3])(:)([0-5]\d)(:[0-5]\d)?$/) || [
+    time,
+  ];
 
-  if (time.length > 1) { // If time format correct
-    time = time.slice(1);  // Remove full string match value
-    time[5] = +time[0] < 12 ? ' AM' : ' PM'; // Set AM/PM
+  if (time.length > 1) {
+    // If time format correct
+    time = time.slice(1); // Remove full string match value
+    time[5] = +time[0] < 12 ? " AM" : " PM"; // Set AM/PM
     time[0] = +time[0] % 12 || 12; // Adjust hours
   }
-  return time.join(''); // return adjusted time or original string
+  return time.join(""); // return adjusted time or original string
 }
 
-
 function logout() {
-
   if (confirm("Are you sure, want to logout!")) {
     sessionStorage.removeItem("username");
     sessionStorage.removeItem("tenant");
     sessionStorage.removeItem("token");
 
-    window.location.href = '/login.html';
+    window.location.href = "/login.html";
   } else {
     return false;
   }
+}
+// function will calculate total pages 
+function calculateTotalPages(totalEntries, pageSize) {
+  let totalpages = Math.ceil(totalEntries / pageSize);
+  return totalpages;
+}
 
+
+//  this will show pagination ui
+
+function showPagination() {
+  let html = `
+     <div class="pagination">
+        <span class="page-span">
+          <span>Show </span>
+          <select  id="pageInfo" onchange="changeSize()">
+
+     ${getOptions()}
+          </select>
+        </span>
+   
+        <div class="pagination_action_buttons">
+          <div>
+        
+          ${getButtons()}
+          </div>
+          </div>
+        </div>`;
+
+  let paginationArea = document.querySelector(".paginationArea");
+
+  paginationArea.innerHTML = html;
+}
+
+function getOptions() {
+  let html = ``;
+  for (item of options) {
+    if (item == pageSize) {
+      html += `<option selected value="${item}" id=option-${item} >${item}</option>`;
+    } else {
+      html += `<option value="${item}" id=option-${item} >${item}</option>`;
+    }
+  }
+
+  return html;
+}
+var previousButton = 1;
+function getButtons() {
+  let html = ``;
+  for (let i = 0; i < totalPages; i++) {
+    if (i + 1 == currentPage) {
+      html += `    <button class="active" id= button-${
+        i + 1
+      } onclick="changePage(${i + 1})">${i + 1}</button>`;
+    } else {
+      html += `    <button id= button-${i + 1} onclick="changePage(${i + 1})">${
+        i + 1
+      }</button>`;
+    }
+  }
+  return html;
+}
+
+function changePage(page) {
+  currentPage = +page;
+
+  fetchMessages(currentPage, pageSize, searchQuery);
+}
+
+function changeSize() {
+  currentPage = 1;
+  let currentItem = document.getElementById("pageInfo");
+  pageSize = +currentItem.value;
+
+  fetchMessages(currentPage, pageSize, searchQuery);
+}
+// show and hide spinner
+function showSpinner() {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "block";
+}
+function hideSpinner() {
+  const spinner = document.getElementById("spinner");
+  spinner.style.display = "none";
 }
